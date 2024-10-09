81a337f052ca4f187bdcf9e755b7db6f181665a0
joel-hudson-27 <jhudson9727@gmail.com>
2024-10-09 15:35:44 -0400

    Parser created and shunting yard algo implemented
    Tokenizes needs a rewrite with supporting functions.
    Tokenizer will split string input into array based
    on matching regex for each type of token.

--- calc.js
+++ calc.js
@@ -1,86 +1,210 @@
-function validRightParen(expression) {
-    let leftCount = 0;
-    let rightCount = 0;
-    for (let i = 0; i < expression.length; i++) {
-        if (rightCount > leftCount) {
-            return false;
-        }
-        else if (expression[i] === ")") {
-            rightCount++;
-        }
-        else if (expression[i] === "(") {
-            leftCount++;
-        }
-    }
+// function validRightParen(expression) {
+//     let leftCount = 0;
+//     let rightCount = 0;
+//     for (let i = 0; i < expression.length; i++) {
+//         if (rightCount > leftCount) {
+//             return false;
+//         }
+//         else if (expression[i] === ")") {
+//             rightCount++;
+//         }
+//         else if (expression[i] === "(") {
+//             leftCount++;
+//         }
+//     }
 
-    return (leftCount == rightCount);
-}
+//     return (leftCount == rightCount);
+// }
 
-function trimLeadingZeros(operand) {
-    const decimalPlace = ".";
-    let result = "";
-    if (operand.includes(decimalPlace, 0)) {
-        result = operand.replace(/^0+/, '0');
-    }
-    else {
-        result = operand.replace(/^0+/, '');
-    }
+// function trimLeadingZeros(operand) {
+//     const decimalPlace = ".";
+//     let result = "";
+//     if (operand.includes(decimalPlace, 0)) {
+//         result = operand.replace(/^0+/, '0');
+//     }
+//     else {
+//         result = operand.replace(/^0+/, '');
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-function evaluateExpression(operator, operandA, operandB) {
-    return false;
-}
-
-const buttons = document.getElementsByTagName("button");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", handleEvent);
-}
-
-function updateDisplay(buttonPressedText) {
-    const display = document.getElementById("calc-display_result");
-    display.textContent = buttonPressedText;
-}
-
-function resetCalculator(){
-    displayText = '';
-    updateDisplay(displayText);
-    currentState = 'idle';
-    
-}
-
-function tokenizeExpression(expression){
-    const operatorRegex = /([()+-/*])/g;
-    tokens = expression.split(operatorRegex);
-    return tokens;
-    
-}
-
-function shuntingYard(tokens){
-    let stack = [];
-    let queue = [];
-    for(let i = 0; i < tokens.length; i++){
-        token = tokens[i];
-        
-    }
-}
-
-function createNode(type, value, children = []){
-    return {
-        type: type,
-        value: value,
-        children: children
-    };
-}
-
-const stateMapping = {
-    idle: "inputtingNumber",
-    inputtingNumber: "inputtingOperator",
-    inputtingOperator: "inputtingNumber",
-    evaluating: "result",
-    result: ["inputtingNumber", "inputtingOperator"],
+const tokenTypes = {
+    NUMBER: 'NUMBER',
+    FUNCTION: 'FUNCTION',
+    ADDITION: '+',
+    SUBTRACTION: '-',
+    MULTIPLICATION: '*',
+    DIVISION: '/',
+    EXPONENTIATION: '^',
+    PARENTHESIS_LEFT: '(',
+    PARENTHESIS_RIGHT: ')',
+    MODULUS: 'MOD',
+    PERCENT: '%',
 };
+
+const tokenRegex = [
+    [/^(?:\d+(?:\.\d*)?|\.\d+)/g, tokenTypes.NUMBER],
+    [/[a-z]+/g, tokenTypes.FUNCTION],
+
+
+
+];
+
+const functionList = ['sqrt'];
+
+const operators = {
+    u: {
+        precedence: 4,
+        association: 'right',
+    },
+
+    '^': {
+        precedence: 4,
+        association: 'right',
+    },
+
+    '+': {
+        precedence: 2,
+        association: 'left',
+    },
+
+    '-': {
+        precedence: 2,
+        association: 'left',
+    },
+
+    '*': {
+        precedence: 3,
+        association: 'left',
+    },
+
+    '/': {
+        precedence: 3,
+        association: 'left',
+    },
+
+    '%': {
+        precedence: 3,
+        association: 'left',
+    },
+
+    'mod': {
+        precedence: 3,
+        association: 'left',
+    },
+};
+
+function isFunction(token) {
+    return functionList.includes(token);
+}
+
+// while there are tokens to be read:
+//     read a token
+//     if the token is:
+//     - a number:
+//         put it into the output queue
+//     - a function:
+//         push it onto the operator stack 
+//     - an operator o1:
+//         while (
+//             there is an operator o2 at the top of the operator stack which is not a left parenthesis, 
+//             and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))
+//         ):
+//             pop o2 from the operator stack into the output queue
+//         push o1 onto the operator stack
+//     - a ",":
+//         while the operator at the top of the operator stack is not a left parenthesis:
+//              pop the operator from the operator stack into the output queue
+//     - a left parenthesis (i.e. "("):
+//         push it onto the operator stack
+//     - a right parenthesis (i.e. ")"):
+//         while the operator at the top of the operator stack is not a left parenthesis:
+//             {assert the operator stack is not empty}
+//             /* If the stack runs out without finding a left parenthesis, then there are mismatched parentheses. */
+//             pop the operator from the operator stack into the output queue
+//         {assert there is a left parenthesis at the top of the operator stack}
+//         pop the left parenthesis from the operator stack and discard it
+//         if there is a function token at the top of the operator stack, then:
+//             pop the function from the operator stack into the output queue
+// /* After the while loop, pop the remaining items from the operator stack into the output queue. */
+// while there are tokens on the operator stack:
+//     /* If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses. */
+//     {assert the operator on top of the stack is not a (left) parenthesis}
+//     pop the operator from the operator stack onto the output queue
+
+function tokenizeExpression(expression) {
+    const operatorRegex = /([()+\-*/])/g;
+    const tokens = expression.split(operatorRegex);
+    return tokens;
+}
+
+function parse(tokens) {
+    let operatorStack = [];
+    let outputQueue = [];
+    for (let i = 0; i < tokens.length; i++) {
+        
+        let token = tokens[i];
+
+        //console.log(outputQueue);
+        //console.log(token);
+
+        if (!isNaN(token)) {
+            outputQueue.push(token);
+        }
+        else if (isFunction(token)) {
+            operatorStack.push(token);
+        }
+        else if (Object.keys(operators).includes(token)) {
+            //let stackTop = operatorStack.at(-1);
+            while (operatorStack.length > 0 &&
+            operatorStack.at(-1) !== '(' &&
+                (operators[operatorStack.at(-1)].precedence > operators[token].precedence ||
+                    (operators[operatorStack.at(-1)].precedence == operators[token].precedence) &&
+                    operators[token].association === 'left')) {
+                outputQueue.push(operatorStack.pop());
+            }
+            operatorStack.push(token);
+        }
+        else if (token === '(') {
+            operatorStack.push(token);
+        }
+        else if (token === ')') {
+        
+            while (operatorStack.length > 0 && operatorStack.at(-1) !== '(') {
+                outputQueue.push(operatorStack.pop());
+                
+            }
+            console.assert(operatorStack.at(-1) === '(', 'Invalid Expression - Mismatched Parentheses');
+            operatorStack.pop();
+            if (isFunction(operatorStack.at(-1))) {
+                outputQueue.push(operatorStack.pop());
+            }
+        }
+    }
+    while(operatorStack.length > 0){
+        console.assert(operatorStack.at(-1) !== '(', 'Invalid Expression - Mismatched Parentheses');
+        outputQueue.push(operatorStack.pop());
+    }
+    return outputQueue;
+}
+
+const expr1 = "(3+2)*2";
+console.log(tokenizeExpression(expr1));
+//console.log(parse(tokenizeExpression(expr1)));
+
+function evaluateExpression(outPutQueue) {
+    
+}
+
+// function createNode(type, value, children = []){
+//     return {
+//         type: type,
+//         value: value,
+//         children: children
+//     };
+// }
+
 
 let displayText = '';
 let currentState = 'idle';
@@ -93,11 +217,11 @@ function handleEvent(event) {
         return;
     }
 
-    if (eventText === '='){
+    if (eventText === '=') {
         tokenizeExpression(displayText);
         updateState('result', displayText);
     }
-    
+
 
     switch (currentState) {
         case 'idle':
@@ -167,4 +291,21 @@ function handleResultState(eventType, eventText) {
 function updateState(newState, value = "") {
     currentState = newState;
     updateDisplay(value);
+}
+
+function updateDisplay(buttonPressedText) {
+    const display = document.getElementById("calc-display_result");
+    display.textContent = buttonPressedText;
+}
+
+function resetCalculator() {
+    displayText = '';
+    updateDisplay(displayText);
+    currentState = 'idle';
+}
+
+//adding event listeners to document
+const buttons = document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", handleEvent);
 }
